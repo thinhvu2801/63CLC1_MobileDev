db4e195e75106a44f9f8060cc07203a673fd0aa8
Vũ Minh Thịnh <minhthinhhero@gmail.com>
2023-12-06 09:06:19 +0700

    up giao diện

--- TicTacToeProject/app/src/main/java/com/vmt/tictactoeproject/MainActivity.java
+++ TicTacToeProject/app/src/main/java/com/vmt/tictactoeproject/MainActivity.java
@@ -3,8 +3,16 @@ package com.vmt.tictactoeproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView playerOneScore, PlayerTwoScore, playerStatus;
+    Button [] buttons = new Button[9];
+    Button resetGame;
+    Button quit;
+    int playerOneScoreCount, playerTwoScoreCount, roundCount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
