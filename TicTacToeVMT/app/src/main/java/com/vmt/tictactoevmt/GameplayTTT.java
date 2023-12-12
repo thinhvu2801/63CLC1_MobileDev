ffcde98174486ad1182dab63064e4a003079068d
Vũ Minh Thịnh <minhthinhhero@gmail.com>
2023-12-12 23:05:00 +0700

    new file, copy code from the old and fix table by 4x4

---
+++ TicTacToeVMT/app/src/main/java/com/vmt/tictactoevmt/GameplayTTT.java
@@ -0,0 +1,69 @@
+package com.vmt.tictactoevmt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameplayTTT extends AppCompatActivity implements View.OnClickListener {
+
+    TextView playerOneScore, playerTwoScore, playerStatus;
+    Button[] buttons = new Button[16];
+    Button resetGame;
+    Button quitButton;
+    int playerOneScoreCount, playerTwoScoreCount, roundCount;
+    boolean activePlayer;
+
+    //    empty = 0
+    //    player 1 = 1
+    //    player 2 = 2
+    int[] gameState = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    int[][] winPosition = {
+            {0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}, //row
+            {0, 4, 8, 12}, {1, 5, 9, 13}, {2, 6, 10, 14}, {3, 7, 11, 15}, //collum
+            {0, 5, 10, 15}, {3, 6, 9, 12} //cross
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameplay_ttt);
+        playerOneScore = findViewById(R.id.playerScore1);
+        playerTwoScore = findViewById(R.id.playerScore2);
+        playerStatus = findViewById(R.id.playerStatus);
+        resetGame = findViewById(R.id.buttonReset);
+        quitButton = findViewById(R.id.buttonQuit);
+
+        //Quit Button code
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GameplayTTT.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        for (int i = 0; i < buttons.length; i++) {
+            String buttonID = "btn_" + i;
+            int resourceID = getResources().getIdentifier(buttonID, "id", getPackageName());
+            buttons[i] = findViewById(resourceID);
+            buttons[i].setOnClickListener(this);
+
+        }
+        roundCount = 0;
+        playerOneScoreCount = 0;
+        playerTwoScoreCount = 0;
+        activePlayer = true;
+    }
+
+    @Override
+    public void onClick(View v) {
+//        Log.i("test","clicked!");
+
+    }
+}
